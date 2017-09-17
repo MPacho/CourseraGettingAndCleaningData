45530fa40ff868605e14de80174d9aebfa6b0aac
Magdalena Paszko <mag.paszko@gmail.com>
2017-09-17 22:57:43 +0200

    Add run_analysis.R and Code_Book.md and complete README.md

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# CourseraGettingAndCleaningData
+# Coursera - Getting And Cleaning Data
+
+This project follows the final project assignment for Johns Hopkins University's Getting and Cleaning Data course on Coursera.org
+
+It uses data from Samsung's Human Activity Recognition Using Smartphones study available <a target="_blank" href="http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones">here</a>
+
+During this study certain measurements were taken on 30 <b>subjects</b> (people) doing a set of 6 <b>activities</b> (walking, walking upstairs, walking downstairs, sitting, standing, laying).
+Those measurements were subsequently processed into a set of 561 <b>features</b>.
+
+The run_analysis.R script reads the raw data from the study and does the following manipulations:
+- Combines the individual pieces (subject, activity and features) from two subsets (train and test) together into one data frame
+- Subsets the set of features to only include those being means and standard deviations of measures taken during the study (features with "-mean()" or "-std()" at the end of their names)
+- Replaces activity codes with meaningful labels
+- Executes some cleaning on the features' names. The original features' names seem descriptive enough, given their relatively large lengths. During the course it was suggested that variable names should be put to lower case and that any abbreviations should be expanded. However in this particular case those two transformations would significantly decrease user experience: it would make variable names very long and illegible. This is why the decision has been made to skip those suggestions. Variable name cleaning has been thus limited only to:
+	* getting rid of non-alphanumeric characters
+	* expanding "t" and "f" at the beginning of each variable name into "time" and "freq" respectively for more clarity.
+	* assuring camel case 
+- Aggregates data by subject and activity
+
+The output file is a dataset with averages of features for each activity and subject.
+
+The script assumes that working directory is set to the "UCI HAR Dataset" folder. The analysis has been conducted on Windows OS.
+
+For variable descriptions, please look into the Code Book.
---
+++ run_analysis.R
@@ -0,0 +1,95 @@
+### The following script prepares the data from
+### Human Activity Recognition Using Smartphones study
+### Assumes that working directory is set to the "UCI HAR Dataset" folder 
+### analysis conducted on Windows OS.
+
+library(data.table)
+library(dplyr)
+
+### ------------------------------------------------------------------------
+### (1) Merges the training and the test sets to create one data set
+
+# Read and combine the test and train features datasets together
+X_train <- fread("train\\X_train.txt")
+X_test <- fread("test\\X_test.txt")
+X <- rbind(X_train, X_test)
+# Read features labels and attach them as column names to X
+features <- fread("features.txt", select = 2, col.names = "name")
+names(X) <- features$name
+
+# Read and combine the test and train activity datasets together
+y_train <- fread("train\\y_train.txt")
+y_test <- fread("test\\y_test.txt")
+y <- rbind(y_train, y_test)
+# Label activities
+names(y) <- "activity"
+
+# Read and combine the test and train subject datasets together
+subject_train <- fread("train\\subject_train.txt")
+subject_test <- fread("test\\subject_test.txt")
+subject <- rbind(subject_train, subject_test)
+# Label subjects
+names(subject) <- "subject"
+
+# Combine subject, activity and features datasets together into one dataset
+data <- cbind(subject, y, X)
+
+
+### ------------------------------------------------------------------------
+### (2) Extracts only the measurements on the mean and standard deviation 
+### for each measurement
+
+# Find features that end with -mean() or -std()
+sel_features <- grep("(-mean\\(\\)|-std\\(\\))$", features$name, value=TRUE)
+# Subset the data by retaining only the selected features
+data <- data[, c("subject",  "activity", sel_features), with=FALSE]
+
+
+### ------------------------------------------------------------------------
+### (3) Uses descriptive activity names to name the activities in the data set
+
+# Read the activity labels
+activities <- fread("activity_labels.txt", col.names = c("code", "label"))
+# replace activity codes by labels in the data
+data$activity <- merge(data, activities, by.x="activity", by.y="code")$label
+
+
+### ------------------------------------------------------------------------
+### (4) Appropriately labels the data set with descriptive variable names
+
+# In my opinion the original variable names are descriptive enough, given their 
+# relatively large lengths.
+# During the course it was suggested that variable names should be put to lower case
+# and that any abbriviations should be expanded. However in this particular case
+# those two transformations would significantly decrease user experience: it would
+# make variable names very long and illegible. This is why the decision has been made
+# to skip those suggestions.
+# Variable name cleaning will be thus limited only to:
+# - getting rid of non-alphanumeric characters
+# - expanding "t" and "f" at the beginning of each variable name into "time" and "freq" 
+#   respectively for more clarity.
+# - assuring camel case 
+
+sel_features <- ifelse(substr(sel_features, 1, 1) == "t", 
+                       sub("^.", "time", sel_features), 
+                       ifelse(substr(sel_features, 1, 1) == "f",
+                              sub("^.", "freq", sel_features),
+                              sel_features
+                       )
+                )
+sel_features <- sub("-mean", "-Mean", sel_features)
+sel_features <- sub("-std", "-Std", sel_features)
+sel_features <- gsub("[^a-zA-Z0-9]", "", sel_features)
+
+names(data)[-(1:2)] <- sel_features
+
+### ------------------------------------------------------------------------
+### (5) From the data set in step 4, creates a second, independent tidy data set with
+### the average of each variable for each activity and each subject
+
+tidy_dataset <- data %>% group_by(activity, subject) %>% summarize_all(funs(mean))
+
+# write the result into a file
+write.table(tidy_dataset, "tidy_dataset.txt", row.name=FALSE)
+
+
